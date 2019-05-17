4c43fefb78aa76f9193edcd3f58bd3ec1d193fdd
PetitT <xcthib@hotmail.com>
2019-05-17 15:15:26 +0200

    PlayerHealth

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -7,31 +7,110 @@ public class player : MonoBehaviour
 
     public float speed;
     public float rotateSpeed;
+    public float backSpeed;
+    public int health;
+
+    public GameObject projectile;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
 
-        Vector2 move = new Vector2(horizontal, vertical);
+        // déplacements
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += transform.up * Time.deltaTime * speed;
+        }
 
-        transform.position += (Vector3)move * speed * Time.deltaTime; //déplacement
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position -= transform.up * Time.deltaTime * backSpeed;
+        }
 
-        // Rotate left
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Rotate(0, 0, rotateSpeed);
         }
-        // Rotate right
+
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.Rotate(0, 0, -rotateSpeed);
         }
+
+        // tirer
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+
+        // die
+        if (health <= 0)
+        {
+            Die();
+        }
+
+    }
+
+    private void Shoot()
+    {
+        var launcher = gameObject.transform.position;
+        var rotation = gameObject.transform.rotation;
+        GameObject tentacule = Instantiate(projectile, launcher, rotation);
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+        new WaitForSeconds(3);
+        Debug.Log("Lol U ded");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var YPos = gameObject.transform.position.y;
+        var XPos = gameObject.transform.position.x;
+        switch (collision.gameObject.tag)
+        {
+            case ("VerticalWall"):
+
+                if (XPos > 0)
+                {
+                    var newXPos = XPos * (-1) + 1;
+                    gameObject.transform.position = new Vector3(newXPos, YPos, 0);
+                }
+                else
+                {
+                    var newXPos = XPos * (-1) - 1;
+                    gameObject.transform.position = new Vector3(newXPos, YPos, 0);
+                }
+                break;
+
+            case ("HorizontalWall"):
+
+                if (YPos > 0)
+                {
+                    var newYPos = YPos * (-1) + 1;
+                    gameObject.transform.position = new Vector3(XPos, newYPos, 0);
+                }
+                else
+                {
+                    var newYPos = YPos * (-1) - 1;
+                    gameObject.transform.position = new Vector3(XPos, newYPos, 0);
+                }
+                break;
+
+            case ("Asteroid"):
+                health -= 50;
+                break;
+
+            default:
+                break;
+        }
+        
     }
 }
